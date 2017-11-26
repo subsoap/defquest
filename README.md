932140db0604c630eaf0e3173bc0942ae8739165
Brian Kramer <subsoap@gmail.com>
2017-11-26 00:52:14 -0800

    Progress

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # DefQuest
 Setup, track, and check real world time countdowns based on OS or server time for Defold
 
+DefQuest can be used for Hearthstone style quests, time locked crates / events (to encourage users to return to the game), or energy regeneration times for games with an energy/heart system.
+
 ## Installation
 You can use DefQuest in your own project by adding this project as a [Defold library dependency](http://www.defold.com/manuals/libraries/). Open your game.project file and in the dependencies field under project add:
 
@@ -12,12 +14,36 @@ Once added, you must require the main Lua module in scripts via
 local defquest = require("defquest.defquest")
 ```
 
-DefQuest has DefSave as an optional dependency if you wish to use it.
+DefQuest has DefSave and DefWindow as optional dependencies but they are highly recommended you use them. If you use the Defold API Window callbacks then if you should use DefWindow to set your Window callback function instead of using Defold's Window API directly.
+
+In your update make sure to include
+
+defquest.update(dt)
+
+And in your final make sure to include (before defsave.final())
+
+defquest.final()
 
 ## Usage
+
+If you are using DefSave then you need to make sure you setup the appname and init DefSave before doing anything with DefQuest (see the example).
+
+defquest.add(id, time, data)
+
+You can set the id til nil to generate a random id. When the random id is generated DefQuest will check to make sure an ID with the random ID it generates is not the same as any other IDs.
+
+If you set an ID when adding a quest it will overwrite any existing quest with that ID.
+
+Time is a table with all values optional but you should set at least one of the values unless you want the quest to complete instantly for some reason. The table values are: seconds, minutes, hours, days, years. 
+
+defquest.add(nil, {hours = 1}, {prize = 25})
+
+## Information
+
+DefQuest is meant for games which have frequent or always on Internet connections. You can use DefQuest offline, but it defeats its purpose.
+
+By default, DefQuest saves its data (using DefSave) to a "defquest" file. You can set the defquest.defsave_filename string to another file, such as your user profile/game data so that users cannot simply swap their defquest files to cheese quest completion over and over.
 
 Beware of setting times based on "midnight" or "noon" in your local time zone, because the user can change their local device timezone and time at will and use that to time travel. It's better to set times based on amounts of time away from current time so that the future time is timezone agnostic.
 
 Beware of using Defold's build in date/time functions as they by default work on local timezone.
-
-##
--- defquest/defquest.lua
+++ defquest/defquest.lua
@@ -1,6 +1,7 @@
 --
 local M = {}
 M.ntp = require("defquest.ntp")
+M.mt = require("defquest.mt")
 
 M.time_now = 0
 
@@ -15,10 +16,14 @@ M.retry_counter = 0
 M.retry_timer = 10
 M.retry_attempts = 0
 M.retry_attempts_max = -1
-M.verbose = false -- if true then successful connection events will be printed, if false only errors
+M.verbose = true -- if true then successful connection events will be printed, if false only errors
 M.use_server_time = true -- if true then NTP servers will be used to sync the current time with, if not then local time will be used only
-M.allow_local_time = false -- if true then if NTP servers can't be reached then local time will be synced
+M.allow_local_time = false -- if true then if NTP servers can't be reached then local time will be synced (could have BAD results)
 M.defsave_filename = "defquest"
+M.keep_finalized = false -- set to false if you want finalized quests not stored in a finalized table, otherwise they are lost forever once game session closes
+M.check_timer = 60 -- number of seconds in between automatic checks to see if any quests are finished
+M.check_timer_counter = 0 -- current check counter value in seconds
+
 
 function M.window_focus_update(self, event, data)
 	if event == window.WINDOW_EVENT_FOCUS_GAINED then
@@ -28,6 +33,7 @@ end
 
 function M.init()
 	M.sync_ntp()
+	M.mt.seed_mt(os.time())
 	
 	if M.use_defsave == true then
 		M.defsave = require("defsave.defsave")
@@ -42,6 +48,14 @@ function M.init()
 		M.defwindow.add_listener(M.window_focus_update)
 	end
 	
+end
+
+function M.generate_random_id()
+	local number = M.mt.random(1, 1000000)
+	while M.quests["random_id_" .. tostring(number)] ~= nil do
+		number = M.mt.random(1, 1000000)
+	end
+	return "random_id_" .. tostring(number)
 end
 
 function M.add(id, time, data)
@@ -63,16 +77,34 @@ function M.add(id, time, data)
 	quest.end_time = time_now
 	quest.data = data
 	if id == nil then
-		table.insert(M.quests, quest)
+		M.quests[M.generate_random_id()] = quest
 	else
 		M.quests[id] = quest
 	end
 	M.defsave.set(M.defsave_filename, "defquest", M.quests )
-	pprint(M.defsave.get(M.defsave_filename, "defquest"))
+	--pprint(M.defsave.get(M.defsave_filename, "defquest"))
 	--pprint(M.quests)
 end
 
+function M.quest_exists(id)
+	if M.quests[id] ~= nil then
+		return true
+	else
+		return false
+	end
+end
+
 function M.mark_finished()
+	local quests_finished = {}
+	for key, value in pairs(M.quests) do
+		if M.quests[key].finished ~= true then
+			if M.quests[key].end_time <= M.time_now then
+				M.quests[key].finished = true
+				table.insert(quests_finished, key)
+			end
+		end
+	end
+	return quests_finished
 end
 
 function M.get_finished(limit)
@@ -106,6 +138,13 @@ function M.sync_ntp()
 end
 
 function M.update(dt)
+	M.check_timer_counter = M.check_timer_counter + dt
+	if M.check_timer_counter >= M.check_timer then
+		M.check_timer_counter = M.check_timer_counter - M.check_timer
+		local quests_finished = M.mark_finished()
+		if M.verbose == true then print("DefQuest: Checking for any finished quests... " .. tostring(#quests_finished) ) end
+	end
+	
 	M.time_now = M.time_now + dt
 	if M.disconnected == true then
 		M.retry_counter = M.retry_counter + dt
